9e19b6f6d73a3da275dff1fcc64b8ba1abf4009a
jgalg <jgalganov@gmail.com>
2017-05-14 23:14:29 -0400

    PDZ trials log

---
+++ pdz_trials_log.py
@@ -0,0 +1,30 @@
+
+# - distance criterion of 6 Angstroms, using nativecontacts (Amber14)
+
+10 trials with 100000 selections each
+trial: 0 -- successes: 44215 | success rate:  44.214999999999996%
+trial: 1 -- successes: 44251 | success rate:  44.251000000000005%
+trial: 2 -- successes: 44442 | success rate:  44.442%
+trial: 3 -- successes: 44426 | success rate:  44.426%
+trial: 4 -- successes: 44281 | success rate:  44.281%
+trial: 5 -- successes: 44218 | success rate:  44.218%
+trial: 6 -- successes: 43966 | success rate:  43.966%
+trial: 7 -- successes: 44482 | success rate:  44.482%
+trial: 8 -- successes: 44146 | success rate:  44.146%
+trial: 9 -- successes: 44465 | success rate:  44.464999999999996%
+--- 300.1468069553375 seconds ---
+[Finished in 300.364s]
+
+10 trials with 1000000 selections each
+trial: 0 -- successes: 442729 | success rate:  44.2729%
+trial: 1 -- successes: 442630 | success rate:  44.263000000000005%
+trial: 2 -- successes: 442575 | success rate:  44.2575%
+trial: 3 -- successes: 443048 | success rate:  44.3048%
+trial: 4 -- successes: 443571 | success rate:  44.3571%
+trial: 5 -- successes: 442746 | success rate:  44.2746%
+trial: 6 -- successes: 442877 | success rate:  44.2877%
+trial: 7 -- successes: 442113 | success rate:  44.211299999999994%
+trial: 8 -- successes: 442235 | success rate:  44.2235%
+trial: 9 -- successes: 442750 | success rate:  44.275%
+--- 2974.7804391384125 seconds ---
+[Finished in 2975.213s]
